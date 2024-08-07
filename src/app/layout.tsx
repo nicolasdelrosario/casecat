a9c704e01b6e018ab851cb758a3b572ec5ba09ec
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-08-06 20:03:08 -0500

    feat: :sparkles: update layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 import { metadata } from './metadata'
 import { Poppins } from 'next/font/google'
 
+import { Footer, Navbar } from '@/components'
+
 import './globals.css'
+
+import { cn } from '@/lib/utils'
+
+export { metadata }
 
 const poppins = Poppins({
 	subsets: ['latin'],
 	weight: ['100', '200', '300', '400', '500', '600', '800', '900'],
 })
-
-export { metadata }
 
 interface RootLayoutProps {
 	children: React.ReactNode
@@ -17,7 +21,13 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang='en'>
-			<body className={poppins.className}>{children}</body>
+			<body className={cn('min-h-screen', poppins.className)}>
+				<header className='sticky inset-x-0 top-0 z-[100] h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+					<Navbar />
+				</header>
+				<main>{children}</main>
+				<Footer />
+			</body>
 		</html>
 	)
 }
