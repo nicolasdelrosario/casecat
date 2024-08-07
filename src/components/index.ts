0f27441d0451a5a00785c5e9b9d4b1110fab8c33
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-08-06 19:53:52 -0500

    feat: :sparkles: add export components file

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Footer } from './Footer'
+export { default as Icons } from './Icons'
+export { default as MaxWidthWrapper } from './MaxWidthWrapper'
+export { default as Navbar } from './Navbar'
+export { default as Phone } from './Phone'
+export { default as Reviews } from './Reviews'
